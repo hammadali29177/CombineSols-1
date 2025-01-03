757b482dec53ce5a47442a24281c543bdd3a55c5
Hammad <hammadali29177@gmail.com>
2025-01-03 15:31:50 +0500

    testing changes

--- FrontEnd/src/components/Home/Home.jsx
+++ FrontEnd/src/components/Home/Home.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Home = () => {
     return (
-        <div className='bg-black text-yellow-300 text-center'>Home</div>
+        <>
+            <div className='bg-black text-yellow-300 text-center'>Home</div>
+            <div className='bg-black text-yellow-300 text-center'>Home</div>
+        </>
     )
 }
 
